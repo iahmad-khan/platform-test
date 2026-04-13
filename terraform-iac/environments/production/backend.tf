32bf229b400be556df4bf287af5e2ed7f4d57b6d
Ijaz Ahmad <ijazahmad722@gmail.com>
2026-04-13 13:10:45 +0500

    update workflows

--- terraform-iac/environments/production/backend.tf
+++ terraform-iac/environments/production/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket       = "platform-test-terraform-state"
-    key          = "production/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true # Native S3 locking (No DynamoDB needed!)
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket       = "platform-test-terraform-state"
+#    key          = "production/terraform.tfstate"
+#    region       = "us-east-1"
+#    use_lockfile = true # Native S3 locking (No DynamoDB needed!)
+#  }
+#}
--- terraform-iac/environments/staging/backend.tf
+++ terraform-iac/environments/staging/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket       = "platform-test-terraform-state"
-    key          = "staging/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true # Native S3 locking (No DynamoDB needed!)
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket       = "platform-test-terraform-state"
+#    key          = "staging/terraform.tfstate"
+#    region       = "us-east-1"
+#    use_lockfile = true # Native S3 locking (No DynamoDB needed!)
+#  }
+#}
