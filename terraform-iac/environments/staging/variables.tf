9e44a00854491196c2b1c40ef876b88a272922a3
Ijaz Ahmad <ijazahmad722@gmail.com>
2026-04-13 13:30:47 +0500

    fix provider and versions

--- terraform-iac/environments/staging/variables.tf
+++ terraform-iac/environments/staging/variables.tf
@@ -1,1 +1,11 @@
-# staging env vars
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
